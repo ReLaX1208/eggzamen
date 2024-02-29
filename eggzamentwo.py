a25dbedfbc1d3aca06903bba57e3ff697f8015b0
ReLaX1208 <daniyal2007@mail.ru>
2024-02-29 22:37:49 +0600

    eggzamen

--- eggzamentwo.py
+++ eggzamentwo.py
@@ -1,5 +1,21 @@
-text = "Осенью лес такой красивый особенно когда ходишь по нему видишь что осень это такая пора которая лишь на мгновение! "
-def bukva(buk):
-    buk.find("a")
+import re 
+def let(let):
+    if not re.match(r'[а-я]', let):
+        return False
+    return True
+def rep(text, let, noun, ad, verb):
+    wor = re.findall(r'\b[' + let + ']+\w*', text)
+    if len(wor) < 3:
+        return text
+    wor = wor[:3]
+    for word in wor:
+        text = re.sub(r'\b' + word + r'\w*',''.join([noun, ad, verb]), text)
+        return text
 if __name__ == "__main__":
-    bukva("о")
+    text = "В лесу так хорошо, легко. Вышла ласка и начала прыгать"
+    let = input("Bukva")
+    noun = input("sysh")
+    adj = input("pril")
+    verb = input("glag")
+    texte = rep(text, let, noun, adj, verb)
+    print(texte)
